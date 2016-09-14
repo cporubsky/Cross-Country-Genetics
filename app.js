d633959004b8e53ce4f3a76f650fc4240143c7b6
Corey Porubsky <cporubsky@ksu.edu>
2016-09-14 07:03:19 -0500

    Added middleware, some node modules, and a route

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    app = express(),
+    sessions = require('client-sessions'),
+    load_user = require('./middleware/load_user'),
+    admin_only = require('./middleware/admin_only'),
+    no_guests = require('./middleware/no_guests'),
+    PORT = 80;
+
+    var session = require('./endpoints/session');
+    app.get('/login', session.login);
+
+    //start express app
+    app.listen(PORT, () => {
+      console.log("Listening on port " + PORT);
+    });
