83bec5498013c75aa9b4fca249999c93928a8a09
Corey Porubsky <cporubsky@ksu.edu>
2016-09-15 23:09:53 -0500

    Log in function works correctly, redirects to plain index page

--- app.js
+++ app.js
@@ -6,8 +6,31 @@
     no_guests = require('./middleware/no_guests'),
     PORT = 80;
 
+    app.set('view engine', 'ejs');   //set view to ejs
+    app.set('views', './templates'); //set templates directory
+
+    //create sessions cookie
+    app.use(sessions({
+      cookieName: 'session',
+      secret: 'somerandomstring',
+      duration: 24*90*60*1000,
+      activeDuration: 1000*60*5
+    }));
+
+    //load user
+    app.use(load_user);
+
+    //set static directory
+    app.use(express.static('public'));
+
+
     var session = require('./endpoints/session');
-    app.get('/login', session.login);
+    app.get('/login', session.login);      //user login form
+    app.post('/login', session.start);  //create session
+    app.get('/logout', session.stop); //deletes session
+
+    var start = require('./endpoints/main');
+    app.get('/index', start.index);
 
     //start express app
     app.listen(PORT, () => {
