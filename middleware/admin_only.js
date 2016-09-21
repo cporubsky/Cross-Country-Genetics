89fcfd762407701bfa5f212852fcff4617726410
Corey Porubsky <cporubsky@ksu.edu>
2016-09-21 10:30:44 -0500

    Redirect non-admin to login page

--- middleware/admin_only.js
+++ middleware/admin_only.js
@@ -4,7 +4,7 @@
 // down the pipeline
 function adminOnly(req, res, next) {
   if(req.user && req.user.is_admin) return next();
-  else res.sendStatus('403');
+  else res.render('session/login', {title: "", message: "You must be an admin to access that page", user: req.user});
 }
 
 module.exports = exports = adminOnly;
