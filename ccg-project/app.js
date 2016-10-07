3ef842e9db6fdb4955df7b576db152218ffb578d
Markloev <markloev@gmail.com>
2016-10-07 09:58:19 -0500

    Added rough jquery and browserify

--- ccg-project/app.js
+++ ccg-project/app.js
@@ -6,7 +6,7 @@
  * @version 1.0
  */
 
- 
+
 
 var express = require('express'),
     app = express(),
---
+++ ccg-project/client/formAbc.js
@@ -0,0 +1,97 @@
+//Pop-up calendar to select a date from
+$("#viewPdf").click(function(){
+  alert();
+});
+
+$( function() {
+$( ".datepicker" ).datepicker();
+} );
+
+//Adds rows to the B table
+$("#plusB").click(function(){
+var rows = document.getElementById("tableB").rows.length;
+var index = 0;
+if(rows < 13){
+  var array = ["number", "earTag", "regNum", "tattoo", "side", "breedCode", "daysSinceEstrus", "stageCode", "qualityCode", "embyroDivided", "commentsB", "results"];
+  var newRow = document.getElementById("tableB").insertRow(rows);
+  for(var i=0;i<13;i++){
+    if(i==0){
+      var td = newRow.insertCell(i);
+      index--;
+    }
+    else{
+      var td = newRow.insertCell(i);
+      var textbox = document.createElement("input");
+      textbox.id = array[index] + rows;
+      textbox.name = array[index] + rows;
+      textbox.class = "smallLine";
+      textbox.type = "text";
+      td.appendChild(textbox);
+      if (i==11){
+        $(td).attr("colspan",2);
+      }
+    }
+    index++;
+  }
+}
+else{
+  alert("Cannot have more than 12 rows");
+}
+});
+
+//Subtracts rows from the B table
+$("#minusB").click(function(){
+var rows = document.getElementById("tableB").rows.length;
+if(rows>2){
+  document.getElementById("tableB").deleteRow(rows-1);
+}
+else{
+alert("Must have at least one row");
+}
+});
+
+//Adds rows to the C table
+$("#plusC").click(function(){
+var rows = document.getElementById("tableC").rows.length;
+var index = 0;
+if(rows < 13){
+  var array = ["caneNum", "strAmpNum", "numEmbryos", "numXWashed", "stageCode", "qualityCode", "zoneIntact", "divided", "trypsinRinsed", "commentsC"];
+  var newRow = document.getElementById("tableC").insertRow(rows);
+  for(var i=0;i<11;i++){
+    if(i==0){
+      var td = newRow.insertCell(i);
+      index--;
+    }
+    else{
+      var td = newRow.insertCell(i);
+      var textbox = document.createElement("input");
+      textbox.id = array[index] + rows;
+      textbox.name = array[index] + rows;
+      textbox.class = "smallLine";
+      textbox.type = "text";
+      td.appendChild(textbox);
+      if (i==1){
+        $(td).attr("colspan",2);
+      }
+      if (i==10){
+        $(td).attr("colspan",2);
+      }
+    }
+    index++;
+  }
+}
+else{
+  alert("Cannot have more than 12 rows");
+}
+});
+
+//Subtracts rows from the C table
+$("#minusC").click(function(){
+var rows = document.getElementById("tableC").rows.length;
+if(rows>2){
+  document.getElementById("tableC").deleteRow(rows-1);
+}
+else{
+alert("Must have at least one row");
+}
+});
