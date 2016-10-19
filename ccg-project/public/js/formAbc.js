872ba5eb06f4838c17556624e70862601d470f50
Markloev <markloev@gmail.com>
2016-10-19 11:19:07 -0500

    PDF changes

--- ccg-project/public/js/formAbc.js
+++ ccg-project/public/js/formAbc.js
@@ -1,15 +1,46 @@
 "use strict";
 
 $("#viewPdf").click(function(){
-  var PDFDocuments = require('pdfkit');
+  var PDFDocument = require('pdfkit');
   var fs = require('fs');
-  doc = new PDFDocument();
-
-  doc.pipe(fs.createWriteStream('formAbc.pdf'));
+  var blobStream  = require('blob-stream');
+  var doc = new PDFDocument();
+  var stream = doc.pipe(blobStream());
+  
+  //doc.pipe(fs.createWriteStream('formAbc.pdf'));
 //  doc.pipe res; # HTTP response
 
-  doc.end();
-
+  //doc.end();
+  // draw some text
+doc.fontSize(25)
+   .text('Here is some vector graphics...', 100, 80);
+   
+// some vector graphics
+doc.save()
+   .moveTo(100, 150)
+   .lineTo(100, 250)
+   .lineTo(200, 250)
+   .fill("#FF3300");
+   
+doc.circle(280, 200, 50)
+   .fill("#6600FF");
+   
+// an SVG path
+doc.scale(0.6)
+   .translate(470, 130)
+   .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+   .fill('red', 'even-odd')
+   .restore();
+   
+// and some justified text wrapped into columns
+doc.text('And here is some wrapped text...', 100, 300);
+   
+   
+// end and display the document in the iframe to the right
+doc.end();
+//stream.on('finish', function() {
+ // iframe.src = stream.toBlobURL('application/pdf');
+//});
 });
 
 $(function() {
