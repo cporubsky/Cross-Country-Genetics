37df0bea16e598f3bfd892fac64b1f6b1bfdf910
Markloev <markloev@gmail.com>
2017-03-15 23:42:31 -0500

    Individual Donor File Submission

--- ccg-enhanced/seed.js
+++ ccg-enhanced/seed.js
@@ -74,20 +74,22 @@ var encryption = require('./encryption.js'),
    client table
    ************/
    // Drop client table if exists
-   db.run("DROP TABLE IF EXISTS client");
+   /*db.run("DROP TABLE IF EXISTS client");
 
    // Create the client table
-   db.run("CREATE TABLE client (id INTEGER PRIMARY KEY AUTOINCREMENT, clientName TEXT, clientAddress TEXT)");
+   db.run("CREATE TABLE client (id INTEGER PRIMARY KEY AUTOINCREMENT, clientName TEXT, clientAddress TEXT, clientPhone TEXT)");
 
    // Create client test data
-   db.run("INSERT INTO client (clientName, clientAddress) values (?,?)",
+   db.run("INSERT INTO client (clientName, clientAddress, clientPhone) values (?,?,?)",
       'Mushroom Red Angus',
-      'Strong City, KS'
+      'Strong City, KS',
+      '7878787878'
    );
-   db.run("INSERT INTO client (clientName, clientAddress) values (?,?)",
+   db.run("INSERT INTO client (clientName, clientAddress, clientPhone) values (?,?,?)",
       'AJ Cabanatuan',
-      '808 Street, Manhattan, KS'
-   );
+      '808 Street, Manhattan, KS',
+      '8673262927'
+   );*/
 
    // Log contents of the client table to the console
    db.each("SELECT * FROM client", function(err, row){
@@ -99,26 +101,52 @@ var encryption = require('./encryption.js'),
     donor table
     ***********/
     // Drop donor table if exists
-    db.run("DROP TABLE IF EXISTS donor");
+    /*db.run("DROP TABLE IF EXISTS donor");
 
     // Create the donor table
-    db.run("CREATE TABLE donor (id INTEGER PRIMARY KEY AUTOINCREMENT, donorClientId INTEGER, donorBreed TEXT, donorRegNum INTEGER, donorTag TEXT, donorName TEXT, FOREIGN KEY(donorClientId) REFERENCES client(id))");
+    db.run("CREATE TABLE donor (id INTEGER PRIMARY KEY AUTOINCREMENT, donorClientId INTEGER, donorBreed TEXT, donorRegNum INTEGER, donorTag TEXT, donorName TEXT, " +
+       "donorLocation TEXT, donorArrival TEXT, donorDeparture TEXT, donorCalfSex TEXT, donorCalfDOB TEXT, donorDOB TEXT, donorRE TEXT, donorMT TEXT, donorLE TEXT, donorMT2 TEXT, donorBrand TEXT, " +
+       "FOREIGN KEY(donorClientId) REFERENCES client(id))");
 
     // Create donor test data
-    db.run("INSERT INTO donor (donorClientId, donorBreed, donorRegNum, donorTag, donorName) values (?,?,?,?,?)",
+    db.run("INSERT INTO donor (donorClientId, donorBreed, donorRegNum, donorTag, donorName, donorLocation, donorArrival, donorDeparture, " +
+    "donorCalfSex, donorCalfDOB, donorDOB, donorRE, donorMT, donorLE, donorMT2, donorBrand) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
        1,
        'AR',
        1546741,
        'Z040',
-       'Mushrush Lana'
+       'Mushrush Lana',
+       'Manhattan',
+       '12/24/16',
+       '12/24/16',
+       'Male',
+       '3/10/17',
+       '12/01/16',
+       'sdfsdf',
+       'sfsfsf',
+       'erwrwra',
+       'sfsfsf',
+       'Branded'
     );
-    db.run("INSERT INTO donor (donorClientId, donorBreed, donorRegNum, donorTag, donorName) values (?,?,?,?,?)",
+    db.run("INSERT INTO donor (donorClientId, donorBreed, donorRegNum, donorTag, donorName, donorLocation, donorArrival, donorDeparture, " +
+    "donorCalfSex, donorCalfDOB, donorDOB, donorRE, donorMT, donorLE, donorMT2, donorBrand) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
        2,
        'BR',
        1234567,
        'AC16',
-       'What is Beef?'
-    );
+       'What is Beef?',
+       'Topeka',
+       '12/22/16',
+       '12/22/16',
+       'Female',
+       '2/24/17',
+       '12/07/16',
+       'sdfsdf',
+       'sfsfsf',
+       'erwrwra',
+       'sfsfsf',
+       'Branded'
+    );*/
 
     // Log contents of the donor table to the console
      db.each("SELECT * FROM donor", function(err, row){
@@ -126,28 +154,28 @@ var encryption = require('./encryption.js'),
        console.log(row);
      });
 
-   /**********
-   bullSelection table
-   ***********/
-   // Drop bullSelection table if exists
-   db.run("DROP TABLE IF EXISTS bullSelection");
+    /**********
+    bullSelection table
+    ***********/
+    // Drop bullSelection table if exists
+    /*db.run("DROP TABLE IF EXISTS bullSelection");
 
-   // Create the bullSelection table
-   db.run("CREATE TABLE bullSelection (id INTEGER PRIMARY KEY AUTOINCREMENT, bullSelectionDonorTag TEXT, bullSelectionCollectionNum TEXT, bullSelectionEmbryoDisposition TEXT, FOREIGN KEY(bullSelectionDonorTag) REFERENCES donor(donorTag))");
+    // Create the bullSelection table
+    db.run("CREATE TABLE bullSelection (id INTEGER PRIMARY KEY AUTOINCREMENT, bullSelectionDonorTag TEXT, bullSelectionCollectionNum TEXT, bullSelectionEmbryoDisposition TEXT, FOREIGN KEY(bullSelectionDonorTag) REFERENCES donor(donorTag))");
 
-   // Create bullSelection test data
-   db.run("INSERT INTO bullSelection (bullSelectionDonorTag, bullSelectionCollectionNum, bullSelectionEmbryoDisposition) values (?,?,?)",
+    // Create bullSelection test data
+    db.run("INSERT INTO bullSelection (bullSelectionDonorTag, bullSelectionCollectionNum, bullSelectionEmbryoDisposition) values (?,?,?)",
       'Z040',
       'Power Eye',
       'Bad disposition'
-   );
-   db.run("INSERT INTO bullSelection (bullSelectionDonorTag, bullSelectionCollectionNum, bullSelectionEmbryoDisposition) values (?,?,?)",
+    );
+    db.run("INSERT INTO bullSelection (bullSelectionDonorTag, bullSelectionCollectionNum, bullSelectionEmbryoDisposition) values (?,?,?)",
       'AC16',
       'Broker',
       'Good disposition'
-   );
+    );*/
 
-   // Log contents of the bullSelection table to the console
+    // Log contents of the bullSelection table to the console
     db.each("SELECT * FROM bullSelection", function(err, row){
       if(err) return console.error(err);
       console.log(row);
@@ -157,7 +185,7 @@ var encryption = require('./encryption.js'),
     treatment table
     ***********/
     // Drop treatment table if exists
-    db.run("DROP TABLE IF EXISTS treatment");
+    /*db.run("DROP TABLE IF EXISTS treatment");
 
     // Create the treatment table
     db.run("CREATE TABLE treatment (id INTEGER PRIMARY KEY AUTOINCREMENT, treatmentDonorTag TEXT, teatmentDate TEXT, treatmentRightOvary TEXT, treatmentLeftOvary TEXT, treatmentUT TEXT, treatmentTubbNum INTEGER, treatmentComments TEXT, FOREIGN KEY(treatmentDonorTag) REFERENCES donor(donorTag))");
@@ -180,7 +208,7 @@ var encryption = require('./encryption.js'),
       'UT Test',
       7,
       'Testing comment'
-    );
+    );*/
 
     // Log contents of the treatment table to the console
      db.each("SELECT * FROM treatment", function(err, row){
@@ -192,7 +220,7 @@ var encryption = require('./encryption.js'),
    sire table
    **********/
    // Drop sire table if exists
-   db.run("DROP TABLE IF EXISTS sire");
+   /*db.run("DROP TABLE IF EXISTS sire");
 
    // Create the sire table
    db.run("CREATE TABLE sire (id INTEGER PRIMARY KEY AUTOINCREMENT, sireDonorId INTEGER, sireName TEXT, sireBreed TEXT, sireRegNum INTEGER, sireCode TEXT)");
@@ -211,7 +239,7 @@ var encryption = require('./encryption.js'),
       'AR',
       7654321,
       '17AR2017'
-   );
+   );*/
 
    // Log contents of the sire table to the console
    db.each("SELECT * FROM sire", function(err, row){
@@ -223,61 +251,76 @@ var encryption = require('./encryption.js'),
    embryo_recovery table
    *********************/
    // Drop embryo_recovery table if exists
-   db.run("DROP TABLE IF EXISTS embryo_recovery");
+   /*db.run("DROP TABLE IF EXISTS embryo_recovery");
 
    // Create the embryo_recovery table
    db.run("CREATE TABLE embryo_recovery (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
           "embryoDonorId INTEGER, embryoSireId INTEGER, embryoTwoId INTEGER, embryoFreezeDate DATE, " +
-          "embryoEstrusOnsetDate DATE, embryoBreedDate DATE, embryoRecoveryDate DATE, embryoNumRecovered INTEGER, " +
-          "embryoNumTransferred INTEGER, embryoNumFrozen INTEGER, embryoEtCode TEXT, FOREIGN KEY(embryoDonorId) REFERENCES donor(id), " +
+          "embryoEstrusOnsetDate DATE, embryoBreedDate DATE, embryoCollectionNum INTEGER, embryoRecoveryDate DATE, embryoNumRecovered INTEGER, " +
+          "embryoNumTransferred INTEGER, embryoNumFrozen INTEGER, embryoNumDegen INTEGER, embryoNumUnfertil INTEGER, embryoSireName TEXT, embryoNumPreg INTEGER, embryoEtCode TEXT, FOREIGN KEY(embryoDonorId) REFERENCES donor(id), " +
           "FOREIGN KEY(embryoSireId) REFERENCES sire(id), FOREIGN KEY(embryoTwoId) REFERENCES sire(id))");
 
    // Create embryo_recovery test data
    db.run("INSERT INTO embryo_recovery (embryoDonorId, embryoSireId, embryoTwoId, embryoFreezeDate, " +
-          "embryoEstrusOnsetDate, embryoBreedDate, embryoRecoveryDate, embryoNumRecovered, embryoNumTransferred, embryoNumFrozen, embryoEtCode) " +
-          "values (?,?,?,?,?,?,?,?,?,?,?)",
+          "embryoEstrusOnsetDate, embryoBreedDate, embryoCollectionNum, embryoRecoveryDate, embryoNumRecovered, embryoNumTransferred, embryoNumFrozen, embryoNumDegen, embryoNumUnfertil, embryoEtCode, embryoSireName, embryoNumPreg) " +
+          "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
       1,
       1,
       null,
-      '2015-10-12',
+      null,
       '2016-06-28',
       '2016-06-28',
+      1,
       '2016-06-05',
       5,
       5,
       0,
+      1,
+      1,
+      'Cow Test',
+      2,
       'E1153'
    );
    db.run("INSERT INTO embryo_recovery (embryoDonorId, embryoSireId, embryoTwoId, embryoFreezeDate, " +
-          "embryoEstrusOnsetDate, embryoBreedDate, embryoRecoveryDate, embryoNumRecovered, embryoNumTransferred, embryoNumFrozen, embryoEtCode) " +
-          "values (?,?,?,?,?,?,?,?,?,?,?)",
+          "embryoEstrusOnsetDate, embryoBreedDate, embryoCollectionNum, embryoRecoveryDate, embryoNumRecovered, embryoNumTransferred, embryoNumFrozen, embryoNumDegen, embryoNumUnfertil, embryoEtCode, embryoSireName, embryoNumPreg) " +
+          "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
       2,
       2,
       null,
       '2016-01-18',
       '2016-10-17',
       '2016-10-17',
+      2,
       '2016-05-16',
       9,
       9,
       0,
+      0,
+      0,
+      'Cow Test2',
+      3,
       'S0117'
     );
     db.run("INSERT INTO embryo_recovery (embryoDonorId, embryoSireId, embryoTwoId, embryoFreezeDate, " +
-           "embryoEstrusOnsetDate, embryoBreedDate, embryoRecoveryDate, embryoNumRecovered, embryoNumTransferred, embryoNumFrozen, embryoEtCode) " +
-           "values (?,?,?,?,?,?,?,?,?,?,?)",
+           "embryoEstrusOnsetDate, embryoBreedDate, embryoCollectionNum, embryoRecoveryDate, embryoNumRecovered, embryoNumTransferred, embryoNumFrozen, embryoNumDegen, embryoNumUnfertil, embryoEtCode, embryoSireName, embryoNumPreg) " +
+           "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
        2,
        2,
        null,
        '2017-01-18',
        '2017-10-17',
        '2017-10-17',
+       3,
        '2017-05-16',
        8,
        8,
        0,
+       1,
+       1,
+       'Cow Test3',
+       1,
        'S0117'
-     );
+     );*/
 
     // Log contents of the embryo_transfer table to the console
     db.each("SELECT * FROM embryo_recovery", function(err, row){
